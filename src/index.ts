6da3a384c854ea28167be99afac7d0b28b33326a
LorgioRoda <lorgiorodaroca@gmail.com>
2024-12-07 12:03:09 +0100

    feat: :sparkles: improve message

--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,21 @@ async function run(): Promise<void> {
         }
 
         const resolvedDir = path.resolve(buildDir);
-        console.log(`Resolved build directory: ${resolvedDir}`);
 
         const budgetPath = path.resolve(process.env.GITHUB_WORKSPACE || '', 'budget.json');
-        console.log(`Looking for budget.json at: ${budgetPath}`);
 
         if (!fs.existsSync(budgetPath)) {
             throw new Error("Budget file 'budget.json' not found in the root of the project");
         }
 
         const budget = JSON.parse(fs.readFileSync(budgetPath, 'utf-8'));
-        console.log(`Budget file content: ${JSON.stringify(budget)}`);
 
         const jsBudgetKB = budget.jsBudgetKB;
         if (!jsBudgetKB) {
             throw new Error("The 'jsBudgetKB' key is missing in budget.json");
         }
 
-        console.log(`JavaScript budget: ${jsBudgetKB} KB`);
-
         const jsFiles = glob.sync(`${resolvedDir}/**/*.js`, { ignore: ['node_modules/**'] });
-        console.log(`Found JavaScript files: ${jsFiles}`);
 
         if (jsFiles.length === 0) {
             core.warning(`No JavaScript files found in ${resolvedDir}`);
@@ -42,21 +36,21 @@ async function run(): Promise<void> {
         jsFiles.forEach((file) => {
             const stats = fs.statSync(file);
             totalSize += stats.size;
-            console.log(`File: ${file} - Size: ${(stats.size / 1024).toFixed(2)} KB`);
         });
 
         const totalSizeInKB = (totalSize / 1024).toFixed(2);
-        console.log(`Total JavaScript size is: ${totalSizeInKB} KB`);
 
         if (parseFloat(totalSizeInKB) > jsBudgetKB) {
-            core.setFailed(`JavaScript size (${totalSizeInKB} KB) exceeds the budget (${jsBudgetKB} KB)`);
+            core.setFailed(
+                `❌ JavaScript size (${totalSizeInKB} KB) exceeds the budget (${jsBudgetKB} KB). Please optimize your build.`
+            );
         } else {
-            console.log(`JavaScript size is within the budget.`);
+            core.setOutput('js_size_kb', totalSizeInKB);
+            console.log(`✅ JavaScript size is within the budget.`);
         }
 
-        core.setOutput('js_size_kb', totalSizeInKB);
     } catch (error) {
-        core.setFailed(`Action failed with error`);
+        core.setFailed('❌ Action failed with error');
     }
 }
 
