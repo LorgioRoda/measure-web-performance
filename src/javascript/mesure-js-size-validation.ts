2be632b6e88dd1b70fb3d01d40f37c1e95c38ca2
LorgioRoda <lorgiorodaroca@gmail.com>
2024-12-24 19:27:56 +0100

    refactor: add size validation

---
+++ src/javascript/mesure-js-size-validation.ts
@@ -0,0 +1,19 @@
+import * as core from "@actions/core";
+interface SizeValidationInput {
+  totalSizeInKB: string;
+  jsBudgetKB: number;
+}
+
+export const validateJavaScriptSize = ({
+  totalSizeInKB,
+  jsBudgetKB,
+}: SizeValidationInput) => {
+  if (parseFloat(totalSizeInKB) > jsBudgetKB) {
+    core.setFailed(
+      `❌ JavaScript size (${totalSizeInKB} KB) exceeds the budget (${jsBudgetKB} KB). Please optimize your build.`
+    );
+  } else {
+    core.setOutput("js_size_kb", totalSizeInKB);
+    console.log(`✅ JavaScript size is within the budget.`);
+  }
+};
